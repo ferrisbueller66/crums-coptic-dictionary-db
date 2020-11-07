f5d1d8dde67eeec3f7631c249087b2c569828cba
ferrisbueller66 <tmtarpinian@gmail.com>
2020-11-06 20:06:40 -0500

    rollback db, delete migration file for self joins table

--- db/migrate/20201022032424_create_entry_connections.rb
+++
@@ -1,8 +0,0 @@
-class CreateEntryConnections < ActiveRecord::Migration[6.0]
-  def change
-    create_table "entry_connections", :force => true, :id => false  do |t|
-      t.integer "entry_a_id", :null => false
-      t.integer "entry_b_id", :null => false
-    end
-  end
-end
